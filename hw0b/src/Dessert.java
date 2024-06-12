b3085f9917ea8edaa5fb6fae8b5c19c5d6f4e875
Dreta <32196824+Dreta@users.noreply.github.com>
2024-06-12 12:02:05 +0800

    feat: hw1b

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,20 @@
+public class Dessert {
+    public static int numDesserts;
+
+    public int flavor;
+    public int price;
+
+    public Dessert(int flavor, int price) {
+        this.flavor = flavor;
+        this.price = price;
+        numDesserts++;
+    }
+
+    public void printDessert() {
+        System.out.println(flavor + " " + price + " " + numDesserts);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
+}
--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,45 +1,62 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
-    }
-
-    /** Returns the order depending on the customer.
-     *  If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
-     *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
-     *  In any other case, return an empty String[] of size 3. */
-    public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
-    }
-
-    /** Returns the positive difference between the maximum element and minimum element of the given array.
-     *  Assumes array is nonempty. */
-    public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        return new int[] { 1, 2, 3, 4, 5, 6 };
     }
 
     /**
-      * Uses recursion to compute the hailstone sequence as a list of integers starting from an input number n.
-      * Hailstone sequence is described as:
-      *    - Pick a positive integer n as the start
-      *        - If n is even, divide n by 2
-      *        - If n is odd, multiply n by 3 and add 1
-      *    - Continue this process until n is 1
-      */
+     * Returns the order depending on the customer.
+     * If the customer is Ergun, return ["beyti", "pizza", "hamburger", "tea"].
+     * If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
+     * In any other case, return an empty String[] of size 3.
+     */
+    public static String[] takeOrder(String customer) {
+        if (customer.equals("Ergun")) {
+            return new String[] { "beyti", "pizza", "hamburger", "tea" };
+        } else if (customer.equals("Erik")) {
+            return new String[] { "sushi", "pasta", "avocado", "coffee" };
+        }
+        return new String[3];
+    }
+
+    /**
+     * Returns the positive difference between the maximum element and minimum
+     * element of the given array.
+     * Assumes array is nonempty.
+     */
+    public static int findMinMax(int[] array) {
+        return Arrays.stream(array).max().getAsInt() - Arrays.stream(array).min().getAsInt();
+    }
+
+    /**
+     * Uses recursion to compute the hailstone sequence as a list of integers
+     * starting from an input number n.
+     * Hailstone sequence is described as:
+     * - Pick a positive integer n as the start
+     * - If n is even, divide n by 2
+     * - If n is odd, multiply n by 3 and add 1
+     * - Continue this process until n is 1
+     */
     public static List<Integer> hailstone(int n) {
         return hailstoneHelper(n, new ArrayList<>());
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        if (x == 1) {
+            list.add(x);
+            return list;
+        }
+        if (x % 2 == 0) {
+            list.add(x / 2);
+            return hailstoneHelper(x / 2, list);
+        }
+        list.add(3 * x + 1);
+        return hailstoneHelper(3 * x + 1, list);
     }
 
 }
