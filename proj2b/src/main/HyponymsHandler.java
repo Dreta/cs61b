5c9bdd01265718a073815730595a73b5b05f9f83
Dreta <32196824+Dreta@users.noreply.github.com>
2024-07-30 03:56:09 +0000

    complete project 2b

--- proj2b/src/main/HyponymsHandler.java
+++ proj2b/src/main/HyponymsHandler.java
@@ -1,5 +1,11 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 import browser.NgordnetQuery;
 import browser.NgordnetQueryHandler;
 import browser.NgordnetQueryType;
@@ -16,6 +22,16 @@ public class HyponymsHandler extends NgordnetQueryHandler {
         if (q.ngordnetQueryType() != NgordnetQueryType.HYPONYMS) {
             throw new IllegalArgumentException("Invalid query type");
         }
-        return wordNet.hyponyms(q.words().get(0)).toString();
+        Set<String> result = new TreeSet<>();
+        List<Collection<String>> results = new ArrayList<>();
+        for (String word : q.words()) {
+            Collection<String> thisResult = wordNet.hyponyms(word);
+            result.addAll(thisResult);
+            results.add(thisResult);
+        }
+        for (Collection<String> thisResult : results) {
+            result.retainAll(thisResult);
+        }
+        return result.toString();
     }
 }
